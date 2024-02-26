13484a4b0040585119d868dd818e5a97d093f0c9
chadningle <chadningle@gmail.com>
2024-02-26 14:27:38 -0700

    Initial Konnect commit

---
+++ deck/sample-commands.sh
@@ -0,0 +1,7 @@
+# Test Konnectivity
+https -A bearer -a Your-Konnect-Token-Here https://us.api.konghq.com/v2/control-planes/
+deck ping --konnect-token Your-Konnect-Token-Here
+
+# Dump Config
+deck gateway dump --konnect-addr https://us.api.konghq.com --konnect-control-plane-name Demo --konnect-token-file $HOME/.kpat_token
+deck gateway dump --konnect-addr https://us.api.konghq.com --konnect-control-plane-name Demo --konnect-token-file $HOME/.kpat_token --output-file deck.yaml
